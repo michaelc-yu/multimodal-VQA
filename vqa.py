e8b4e45928751e7e1a08675510744dc9493a9615
michaelc-yu <woolythesheep@gmail.com>
2024-07-01 21:30:50 -0700

    add test code and test image

--- vqa.py
+++ vqa.py
@@ -364,23 +364,30 @@ for epoch in range(num_epochs):
 
 torch.save(vqamodel.state_dict(), 'vqamodel.pth')
 
-# vqamodel.load_state_dict(torch.load('vqamodel.pth'))
-# vqamodel.eval()
+vqamodel.load_state_dict(torch.load('vqamodel.pth'))
+vqamodel.eval()
 
-# test_img = "test-images/COCO_train2014_000000086927.jpg"
 
-# user_question = "Are there cars in this photo?"
+test_img = "test-images/COCO_train2014_000000005946.jpg"
 
-# test_img = transform(test_img)
-# img_features = bottom_up_model(test_img)
-# img_features = img_features[0]['boxes']
+test_img = Image.open(test_img).convert('RGB')
+test_img = transform(test_img)
+test_img = test_img.unsqueeze(0).to(device)
+img_features = [bottom_up_model(test_img)[0]['boxes'].to(device)]
+img_features = list_to_tensor(img_features)
 
-# q_tokens = user_question.lower().strip().split()
-# q_indices = [word_to_idx.get(token, word_to_idx["<UNK>"]) for token in q_tokens]
-# q_tensor = torch.tensor(q_indices, dtype=torch.long)
+while True:
+    user_question = input("Enter a question: ")
+    if user_question == "quit":
+        break
+    print(user_question)
+    q_tokens = user_question.lower().strip().split()
+    q_indices = [word_to_idx.get(token, word_to_idx["<UNK>"]) for token in q_tokens]
+    q_tensor = torch.tensor(q_indices, dtype=torch.long).unsqueeze(0).to(device)
 
-# output, _ = vqamodel(img_features, q_tensor)
-# ans_idx = torch.argmax(output, dim=1)
-# ans = answer_vocab[ans_idx]
-# print(f"answer: {ans}")
+    with torch.no_grad():
+        output, _ = vqamodel(img_features, q_tensor)
+        ans_idx = torch.argmax(output, dim=1)
+        ans = common_answers[ans_idx]
+        print(f"answer: {ans}")
 
